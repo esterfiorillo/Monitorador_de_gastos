e5a953efaa9643330ed6bfd24ac588e96e3def6b
AndreOGodoy <godoy.andre@hotmail.com>
2021-07-01 18:07:07 -0300

    Transaction Bar: Fields

--- front/src/components/Barra.vue
+++ front/src/components/Barra.vue
@@ -1,8 +1,12 @@
 <template>
     <div class="transaction-bar">
     <div class = "transaction-form">
-        <input class="transaction-form__input" type="text" name="transaction" placeholder="Digite aqui sua transação">
-
+        <input class="transaction-description" type="text" name="description" placeholder="Digite aqui sua transação">
+        <input class="transaction-value" type="text" name="value" placeholder="Valor (R$)">
+        <select class="transaction-category" name="category"> 
+            <option value="geral">Geral</option>
+            <option value="outros">Outros</option>            
+        </select>
       </div>
     </div>
 </template>
@@ -20,16 +24,38 @@ export default {
   margin-bottom: 40px;
   background-color: #fff;
   width: 600px;
-  height: 44px;
-  border-radius: 2px;
+  height: 50px;
+  display: flex;
+  border: 1px black;
 }
 
-.transaction-form__input {
-  width: 100%;
-  height: 100%;
+.transaction-form:focus {
+    outline-color: black;
+    outline-style: inset;
+}
+
+.transaction-description {
+  width: 50%;
   font-family: sans-serif;
   font-size: 18px;
   color: #7c7c7c;
+}
+
+.transaction-value {
+  width: 30%;
+
+  font-family: sans-serif;
+  font-size: 18px;
+  color: #7c7c7c;
+}
+
+.transaction-category {
+  width: 20%;
+  font-family: sans-serif;
+  font-size: 18px;
+  color: #7c7c7c;
+  background: transparent;
+  border: none;
 }
 
 </style>
