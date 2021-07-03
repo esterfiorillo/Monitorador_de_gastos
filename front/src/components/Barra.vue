b66bd221b6ba5aae39b86a0077f15e8ae1aaa2e5
Pedro <0wfonsecapedro@gmail.com>
2021-07-03 11:17:59 -0300

    botao da barra

--- front/src/components/Barra.vue
+++ front/src/components/Barra.vue
@@ -2,8 +2,9 @@
     <div class="transaction-bar">
     <div class = "transaction-form">
         <input class="transaction-description" type="text" name="description" placeholder="Digite aqui sua transação">
-        <input class="transaction-value" type="text" name="value" placeholder="Valor (R$)">
         <input class="transaction-category" type="text" name="categoria" placeholder="Categoria">
+        <input class="transaction-value" type="text" name="value" placeholder="Valor (R$)">
+        <button class="button button1"> Adicionar </button>
       </div>
     </div>
 </template>
@@ -16,10 +17,31 @@ export default {
 
 <style>
 
+.button {
+  border: none;
+  color: white;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+
+.button1 {
+  background-color: #0080ab; 
+  color: white; 
+}
+
+.button1:hover {
+  background-color: #25c3ff;
+  color: white;
+}
+
 .transaction-form {
   margin: auto;
   margin-bottom: 40px;
-  background-color: #fff;
+  background-color: #5abbbd;
   width: 600px;
   height: 50px;
   display: flex;
@@ -34,22 +56,22 @@ export default {
 .transaction-description {
   width: 50%;
   font-family: sans-serif;
-  font-size: 18px;
+  font-size: 17px;
   color: #7c7c7c;
 }
 
 .transaction-value {
-  width: 30%;
+  width: 20%;
 
   font-family: sans-serif;
-  font-size: 18px;
+  font-size: 17px;
   color: #7c7c7c;
 }
 
 .transaction-category {
-  width: 20%;
+  width: 30%;
   font-family: sans-serif;
-  font-size: 18px;
+  font-size: 17px;
   color: #7c7c7c;
 }
 
